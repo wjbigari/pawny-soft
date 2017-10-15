4cabe18df4234c0867cb0ba0ca3633adb0a81072
adarshk96 <kurian.adarsh66@gmail.com>
2017-10-15 06:05:17 -0500

    completely refactlored UI to use Fragments and Viewpager added main menu

--- app/src/main/java/com/example/ak/mealplanner/MainActivity.java
+++ app/src/main/java/com/example/ak/mealplanner/MainActivity.java
@@ -37,87 +37,23 @@ public class MainActivity extends AppCompatActivity {
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount()));
         tabLayout.setupWithViewPager(viewPager);
-        /**
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-        /**
-        TabHost host = (TabHost)findViewById(R.id.tabHost);
-        host.setup();
-
-        //Tab 1
-        TabHost.TabSpec spec = host.newTabSpec("Builder");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("Builder");
-        host.addTab(spec);
-
-        //Tab 2
-        spec = host.newTabSpec("Profile");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("Profile");
-        host.addTab(spec);
-
-        //Tab 3
-        spec = host.newTabSpec("Calendar");
-        spec.setContent(R.id.tab3);
-        spec.setIndicator("Calendar");
-        host.addTab(spec);
-
-        // Find the ListView resource.
-        ListView mainListView = (ListView) findViewById(R.id.list_main);
-        ListView mainListView2 = (ListView) findViewById(R.id.list_main2);
-        ListView mainListView3 = (ListView) findViewById(R.id.list_main3);
-
-        // Create and populate a List of food names
-        ArrayList<Fooditem> foodList = new ArrayList<Fooditem>();
-
-        // Create ArrayAdapter
-        ArrayAdapter<Fooditem> listAdapter  = new ArrayAdapter<Fooditem>(this, R.layout.listrow, foodList);
-
-        // Add more foods
-        listAdapter.add(new Fooditem("Bread", "10"));
-        listAdapter.add(new Fooditem("Milk", "10"));
-        listAdapter.add(new Fooditem("Eggs", "10"));
-        listAdapter.add(new Fooditem("Oranges", "10"));
-
-        // Set the ArrayAdapter as the ListView's adapter.
-        mainListView.setAdapter(listAdapter);
-        mainListView2.setAdapter(listAdapter);
-        mainListView3.setAdapter(listAdapter);
-
-        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View arg1, int position, long arg3)
-            {
-                Fooditem entry= (Fooditem) parent.getAdapter().getItem(position);
-
-            }
-        });
-
-        */
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     /**
     public void sendMessage(View view) {
