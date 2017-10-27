f87bfe1d0ecb6a61a2008f070ec9a3378d2ca9fb
wjbigari <wjbigari@gmail.com>
2017-10-26 20:40:56 -0500

    results holdover

--- app/src/main/java/com/example/ak/mealplanner/Results.java
+++ app/src/main/java/com/example/ak/mealplanner/Results.java
@@ -1,10 +1,14 @@
 package com.example.ak.mealplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import Controllers.MealPlannerController;
 
@@ -15,13 +19,31 @@ public class Results extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
         TextView text = (TextView) findViewById(R.id.resultview);
-        MealPlannerController mpc = new MealPlannerController();
 
+//        FoodItem pineapple = new FoodItem("Pineapple", 1, 2, "oz.", 82, 21, 1, 0);
+//        FoodItem bread = new FoodItem("Bread", 2, 1, "slice(s)", 60, 12, 3, 1);
+//        MealItem m_pineapple = new MealItem(pineapple, MealItem.Meal.LUNCH);
+//        MealItem m_bread = new MealItem(bread, MealItem.Meal.LUNCH);
+//        ArrayList<MealItem> mi = new ArrayList<MealItem>();
+//        mi.add(m_pineapple);
+//        mi.add(m_bread);
+
+        Intent intent = getIntent();
+        ArrayList<MealItem> mealList = (ArrayList<MealItem>)intent.getSerializableExtra("list");
+        Log.i("adarsh",mealList.toString());
+        Constraints c = new Constraints(
+                2000, 2100,			//Min Cals, Max Cals
+                150, 170,			//Min Carbs, Max Carbs (in g)
+                150, 170,			//Min Prot, Max Prot  (in g)
+                80, 85);
+        MealPlannerController mpc = new MealPlannerController(c,mealList,text);
+        mpc.execute();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 
     @Override
