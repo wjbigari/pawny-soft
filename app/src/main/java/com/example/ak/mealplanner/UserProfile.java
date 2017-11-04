706bb0d6d1637af1def126c0eb8d678b72d818d9
wjbigari <wjbigari@gmail.com>
2017-11-03 21:12:41 -0500

    Controllers for User Insert and Update

--- app/src/main/java/com/example/ak/mealplanner/UserProfile.java
+++ app/src/main/java/com/example/ak/mealplanner/UserProfile.java
@@ -1,5 +1,6 @@
 package com.example.ak.mealplanner;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.Serializable;
@@ -13,9 +14,27 @@ public class UserProfile implements Serializable {
     String username;
     String name;
     int age,height,weight;
-
-    public JSONObject toJSON(){
+    public UserProfile(JSONObject jobject) throws JSONException {
+        this.username = jobject.getString("username");
+        this.name = jobject.getString("name");
+        this.age = jobject.getInt("age");
+        this.height = jobject.getInt("height");
+        this.weight = jobject.getInt("weight");
 
     }
 
+    public String getHeighString(){
+        int feet = this.height/12;
+        int inches = this.height % 12;
+        return "" + feet + "' " + inches + " \"";
+    }
+    public JSONObject toJSON() throws JSONException {
+        JSONObject jobject = new JSONObject();
+        jobject.put("username", this.username);
+        jobject.put("name", this.name);
+        jobject.put("age", this.age);
+        jobject.put("height", this.height);
+        jobject.put("weight", this.weight);
+        return jobject;
+    }
 }
